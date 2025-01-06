d00ca911b413ee52b796a5cd34c78dd924e64df4
ABCCheng <cbestboy@sina.com>
2025-01-06 17:05:25 -0500

    1. tui-editor: support Chinese

--- src/pages/tools/tui-editor/index.ts
+++ src/pages/tools/tui-editor/index.ts
@@ -1,15 +1,14 @@
 import { defineTool } from '../tool';
 import { translate } from '@/plugins/i18n.plugin';
-
-import {photo-edit} from '@tabler/icons-vue';
+import { IconPhotoEdit} from '@tabler/icons-vue';
 
 export const tool = defineTool({
   name: translate('tools.tui-editor.title').value,
   path: '/tui-editor',
   description: translate('tools.tui-editor.description').value,
-  keywords: ['image editor', '图片编辑'],
+  keywords: ['image editor', '图片编辑', '修改图片'],
   component: () => import('./tui-editor.vue'),
-  icon: photo-edit,
-  createdAt: new Date('2024-12-08'),
+  icon: IconPhotoEdit,
+  createdAt: new Date('2024-12-23'),
   badge: 'default',
 });
--- src/pages/tools/tui-editor/tui-editor.vue
+++ src/pages/tools/tui-editor/tui-editor.vue
@@ -2,8 +2,11 @@
 import { ref, onMounted, onBeforeUnmount } from 'vue';
 import ImageEditor from 'tui-image-editor';
 import 'tui-image-editor/dist/tui-image-editor.css';
+import 'tui-color-picker/dist/tui-color-picker.css';
 
 import { IconArrowsMaximize, IconArrowsMinimize, IconDownload, IconFileDots } from '@tabler/icons-vue';
+
+const { locale } = useI18n();
 
 const fullscreenElementRef = ref<HTMLDivElement | null>(null);
 const isFullscreen = ref(false);
@@ -43,6 +46,83 @@ onBeforeUnmount(() => {
   document.removeEventListener('fullscreenchange', handleFullscreenChange);
 });
 
+const locale_zh = {
+    ZoomIn: '放大',
+    ZoomOut: '缩小',
+    Hand: '抓手工具',
+    History: '历史',
+    Undo: '撤销',
+    Redo: '恢复',
+    Reset: '重置',
+    Delete: '删除',
+    DeleteAll: '全部删除',
+    Resize: '尺寸',
+    Width: '宽度',
+    Height: '高度',
+    'Lock Aspect Ratio': '锁定宽高比例',
+    Apply: '应用',
+    Cancel: '取消',
+    Flip: '镜像',
+    'Flip X': 'X 轴',
+    'Flip Y': 'Y 轴',
+    Mask: '蒙版',
+    'Load Mask Image': '上传蒙版图片',
+    Crop: '裁剪',
+    Square: '正方形',
+    Rotate: '旋转',
+    Range: '区间',
+    Draw: '画笔',
+    Free: '曲线',
+    Straight: '直线',
+    Color: '颜色',
+    Shape: '图形',
+    Rectangle: '矩形',
+    Circle: '圆形',
+    Triangle: '三角形',
+    Fill: '填充',
+    Stroke: '描边',
+    Icon: '图标',
+    Arrow: '箭头',
+    'Arrow-2': '箭头2',
+    'Arrow-3': '箭头3',
+    'Star-1': '五角星',
+    'Star-2': '多角形',
+    Polygon: '多边形',
+    Location: '定位',
+    Heart: '心形',
+    Bubble: '气泡',
+    'Custom icon': '自定义图标',
+    Text: '文字',
+    Bold: '加粗',
+    Italic: '斜体',
+    Underline: '下划线',
+    Left: '左对齐',
+    Center: '居中',
+    Right: '右对齐',
+    'Text size': '字体大小',
+    Filter: '滤镜',
+    Grayscale: '灰度',
+    Sepia: '棕色',
+    Blur: '模糊',
+    Emboss: '浮雕',
+    Invert: '底片',
+    Sepia2: '棕色2',
+    Sharpen: '锐化',
+    'Remove White': '除去白色',
+    Distance: '距离',
+    Brightness: '亮度',
+    Noise: '铜板雕刻',
+    Pixelate: '马赛克',
+    'Color Filter': '彩色滤镜',
+    Threshold: '阈值',
+    Tint: '色调',
+    Multiply: '正片叠底',
+    Blend: '混合色',
+    Custom: '自定义',
+    load: '上传',
+    download:'下载',
+  };
+
 
 const initializeEditor = (filePath: string, fileName: string) => {
   const editorRect = editorRef.value.getBoundingClientRect();
@@ -65,6 +145,7 @@ const initializeEditor = (filePath: string, fileName: string) => {
       },
       menu: ['resize', 'crop', 'flip', 'rotate', 'draw', 'shape', 'icon', 'text', 'mask', 'filter'],
       initMenu: '',
+      locale: locale.value==='en' ? '' : locale_zh,
       uiSize: {
         width: `${width - 30}px`,
         height: `${height}px`,
@@ -193,6 +274,14 @@ onMounted(() => {
 
 ::v-deep(.tui-image-editor-header-buttons) {
   display: none !important;
+}
+
+::v-deep(.tui-colorpicker-palette-container ul) {
+  width: 160px !important;
+}
+
+::v-deep(.tui-colorpicker-palette-container li) {
+  padding: 0 !important;
 }
 
 .fullscreen_button {
